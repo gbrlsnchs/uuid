103bc554c20e068b6479e64bab2c918b5cc26c3b
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-30 18:24:40 -0300

    Add version 2 support

---
+++ v2.go
@@ -0,0 +1,20 @@
+package uuid
+
+import "encoding/binary"
+
+// V2 generates a version 2 UUID.
+//
+// It basically returns a version 1 UUID but overrides
+// the least significant 8 bits of the clock sequence by
+// the variable "ldn", a local domain name, and the least significant
+// 32 bits of the timestamp an integer identifier "id".
+func V2(id uint32, ldn uint8) (UUID, error) {
+	guid, err := timestampUUID()
+	if err != nil {
+		return Null, err
+	}
+	binary.BigEndian.PutUint32(guid[:4], id)
+	guid[9] = ldn
+	guid.setVersion(Version2)
+	return guid, nil
+}
