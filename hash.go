6f883f4b84b5a88ac1186bb530e70c7ed8a4c337
Gabriel Sanches <g@lonemaker.com>
2018-10-16 16:40:45 -0300

    Fix namespaces parsing

--- hash.go
+++ hash.go
@@ -4,13 +4,13 @@ import "hash"
 
 var (
 	// NamespaceDNS is the namespace UUID defined for DNS.
-	NamespaceDNS, _ = Parse("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+	NamespaceDNS, _ = Parse([]byte("6ba7b810-9dad-11d1-80b4-00c04fd430c8"))
 	// NamespaceURL is the namespace UUID defined for URL.
-	NamespaceURL, _ = Parse("6ba7b811-9dad-11d1-80b4-00c04fd430c8")
+	NamespaceURL, _ = Parse([]byte("6ba7b811-9dad-11d1-80b4-00c04fd430c8"))
 	// NamespaceOID is the namespace UUID defined for ISO OID.
-	NamespaceOID, _ = Parse("6ba7b812-9dad-11d1-80b4-00c04fd430c8")
+	NamespaceOID, _ = Parse([]byte("6ba7b812-9dad-11d1-80b4-00c04fd430c8"))
 	// NamespaceX500 is the namespace UUID defined for X.500 DN.
-	NamespaceX500, _ = Parse("6ba7b814-9dad-11d1-80b4-00c04fd430c8")
+	NamespaceX500, _ = Parse([]byte("6ba7b814-9dad-11d1-80b4-00c04fd430c8"))
 )
 
 func hashUUID(h hash.Hash, nspace UUID, data []byte) (UUID, error) {
